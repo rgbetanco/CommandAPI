dcd6020a1dbba6bf9abc6965a7f94c83a47c3e48
ronaldtaipay <ronald@taipay.com>
2020-05-20 15:14:05 +0800

    db connection string moved to secret.json

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -6,6 +6,7 @@ using CommandAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,12 +18,16 @@ namespace CommandAPI
     {
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration) => Configuration = configuration;
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CommandContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("serverConnection")));
+            SqlConnectionStringBuilder builder =
+            new SqlConnectionStringBuilder(Configuration.GetConnectionString("serverConnection"));
+            builder.ConnectionString = Configuration["SavedConnectionString"];
+
+            services.AddDbContext<CommandContext>(opt => opt.UseSqlServer(builder.ConnectionString));
             services.AddControllers();
         }
 
